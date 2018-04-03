c6cde0f0da8c8450971f5add3680f70b98d82df6
MatthewKoch76 <matthewkoch76@gmail.com>
2018-04-02 19:57:56 -0500

    first few errors done

--- main.py
+++ main.py
@@ -4,12 +4,41 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/')
+
 def index():
+    return render_template('forms.html')
+
+@app.route('/', methods=['POST'])
+
+def sindex():
+    username = request.form['Username']
+    password = request.form['Password']
+    verify_password = request.form['Verify_Password']
+    email = request.form['Email']
+
+    name_error = ''
+    password_error = ''
+    verify_password_error = ''
+    email_error = ''
+    
+        
+    if not len(username) in range(3,20):
+        name_error = 'please enter a username between 3-20 characters'
+
+    if password != verify_password:
+        verify_password_error = 'passwords must match'
+
     
 
-    return render_template('Username.html')
-
-
-
+    return render_template('forms.html', 
+    Username=username,
+    Password = password,
+    Verify_Password = verify_password,
+    Email = email,
+    name_error=name_error, 
+    password_error=password_error, 
+    verify_password_error=verify_password_error,
+    email_error=email_error,)
+        
 app.run()
 
